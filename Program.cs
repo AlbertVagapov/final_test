148ff26be0fd0abe69d8c0e6fca402e56a0a39e7
Albert Vagapov <va3@list.ru>
2022-04-04 18:00:21 +0500

    add gitignore

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string str = "012wxyz789";
+    char[] arr;
+
+    arr = str.ToCharArray(3, 4);
+    Console.Write("The letters in '{0}' are: '", str);
+    Console.Write(arr);
+    Console.WriteLine("'");
+    Console.WriteLine("Each letter in '{0}' is:", str);
+    foreach (char c in arr)
+        Console.WriteLine(c);
