059037a261db52af94d5a8009eb3c445011c81de
Albert Vagapov <va3@list.ru>
2022-04-05 14:38:39 +0500

    block added

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿string str = "012wxyz789";
-    char[] arr;
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символам. Первоначально массив можно ввести с 
+// клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.  
 
-    arr = str.ToCharArray(3, 4);
-    Console.Write("The letters in '{0}' are: '", str);
-    Console.Write(arr);
-    Console.WriteLine("'");
-    Console.WriteLine("Each letter in '{0}' is:", str);
-    foreach (char c in arr)
-        Console.WriteLine(c);
+//Примеры:
+
+//["Hello", "2", "world", ":-)"] -> ["2", ":-)"]
+
+//["1234", "1567", "-2", "computer science"] -> ["-2"]
+
+//["Russia", "Denmark", "Kazan"] -> []
+
+
+string[] array = new string[8] {"Russia", "way", "Ufa", "aim", "USA", "Mexica", "Canada", "key"};
+string[] secondarray = new string[array.Length];
+void SortArray(string[] array, string[] secondarray)
+{
+    int count = 0;
+    for (int ind = 0; ind < array.Length; ind++)
+    {
+    if(array[ind].Length <= 3)
+        {
+        secondarray[count] = array[ind];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int ind = 0; ind < array.Length; ind++)
+    {
+        Console.Write($"{array[ind]}  ");
+    }
+}
+SortArray(array, secondarray);
+PrintArray(secondarray);
